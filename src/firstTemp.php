0aa97da9ddd4e116d4eec9cc9eb3594411e0837f
Matt Fenwick <mfenwick@student.uchc.edu>
2011-11-19 11:03:57 -0600

    replace tabs with spaces

--- src/firstTemp.php
+++ src/firstTemp.php
@@ -5,14 +5,14 @@ function getConnection() {
     $db_pass = "password";
     $db_name = "database";
 
-	$con = mysql_connect("localhost", $db_user, $db_pass);  
-	
-	if (!$con) {
-		return false;
-	}
-	if(!mysql_select_db($db_name, $con)) {
-		return false;
-	}
+    $con = mysql_connect("localhost", $db_user, $db_pass);  
+    
+    if (!$con) {
+        return false;
+    }
+    if(!mysql_select_db($db_name, $con)) {
+        return false;
+    }
     return $con;
 }
 
