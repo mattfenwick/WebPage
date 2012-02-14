9b6b71bd97bd2342322aef9ef5da8600bc60dba2
Matt Fenwick <mfenwick@student.uchc.edu>
2012-02-14 08:32:20 -0600

    add status tracker; refactor post/get code and success/failure functions; style status tracker

--- src/web/chat/chat.js
+++ src/web/chat/chat.js
@@ -1,49 +1,83 @@
 
+// save messages
+
+function onSaveSuccess(res) {
+     // TODO: put message in "conversation" queue -- or let that be taken care of by 'get'?
+     $("#status").append('<p>saved message: ' + res.success + '</p>');
+}
+
+function onMySaveError(res) {
+     // TODO: whine to user
+     $("#status").append('<p>failed to save message: ' + res.error + '</p>');
+}
+
+function onHttpSaveError(response) {
+    // TODO: whine, give advice to user (i.e. enable javascript, check network, etc.)
+    $("#status").append('<p>http request failed: ' + response + "</p>");
+}
+
 function saveMessage(username, room, text) {
     var request = $.post( 'chat.php', 
             {'username': username,
-    	     'room': room,
-    	     'text': text,
-    	     'type': 'savemessage'}, 
+             'room': room,
+             'text': text,
+             'type': 'savemessage'}, 
             function (response) {
                  var res = JSON.parse(response); //error: no parse ????
                  if("error" in res) {
-                	 // don't put message in "conversation" queue, log error, whine to user
-                	 alert("failed: " + res.error);
+                     onMySaveError(res);
                  } else {
-                	 // put message in "conversation" queue
-                	 // change dialogs to jqueryui
-                	 // "log" the success
-                	 alert("success: " + response);
+                     onSaveSuccess(res);
                  }
             }
     );
-    
-    request.error(function(resp) {
-        alert("http savemessage request failed: " + resp);
-    });
+    request.error(onHttpSaveError);
+}
+
+/*********************************************************************************/
+// get all messages
+
+function onGetSuccess(response) {
+    $("#messagehistory").empty();
+    for(var i = 0; i < response.messages.length; i++) {
+        var m = response.messages[i];
+        $("#messagehistory").append('<div><sub>time: ' + m.time + 
+                                    'user: ' + m.username + '</sub>' + 
+                                    '<div class="message">' + m.text + 
+                                      '</div></div>');
+    }
+    $("#status").append('<p class="success">get messages succeeded, got ' + 
+    		            response.messages.length + ' messages</p>');
+}
+
+function onGetFailure(message, response) {
+	$("#status").append('<p class="failure">get messages failed: ' + 
+			            message + "  " + response + "</p>");
+}
+
+function getAllMessages(room, onFailure) {
+    var request = $.get('chat.php',
+            {'type': 'getallmessages', 'room': room},
+            function (response) {
+                try {
+                    var res = JSON.parse(response);
+                    if("error" in res) {
+                        onGetFailure('failed to retrieve all messages', res);
+                    } else {
+                        onGetSuccess(res);
+                    }
+                } catch(e) {// if the json parse fails
+                    onGetFailure(e, response);//should I extract the message from e?
+                }
+            }
+    );
+    request.error = function(resp) {onGetFailure("http request failed", resp);};
 }
 
 
-function getAllMessages(room, onSuccess) {
-	var request = $.get('chat.php',
-			{'type': 'getallmessages', 'room': room},
-			function (response) {
-				var res = JSON.parse(response);
-				if("error" in res) {
-					alert('failed to retrieve all messages: ' + res.error);
-				} else {
-					onSuccess(res);
-				}
-			}
-	);
-	
-	request.error = function(response) {
-        alert("http getallmessages request failed: " + resp);
-	};
-}
-
+/*********************************************************************************/
+// get some messages
 
 function getMessagesSince(room, time) {
-	
+    
 }
