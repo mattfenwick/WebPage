febe1fd150c82716ac5c94d4c6a0b55d179c80d7
Matt Fenwick <mfenwick@student.uchc.edu>
2012-03-05 15:38:03 -0600

    add rankings data; refactor views

--- src/database/tennis/finals/views.sql
+++ src/database/tennis/finals/views.sql
@@ -2,31 +2,45 @@
 use tennisfinals;
 
 
-
-drop view if exists by_eventtype;
-create view by_eventtype as (
-  select 
-    playerid, 
-    `type` as `event type`, sum(isvictory) as wins, 
-    count(*) - sum(isvictory) as losses, count(*) as total
-  from final 
-  group by playerid, `type`
-);
-
-
+-- this is a base view
+--   many of the views built on this view
+--   assume that each "name" is unique
 
 drop view if exists joined_player;
 create view joined_player as (
   select 
-    final.*,
-    p1.name,
-    p2.name as `opponent name`,
-    p2.country as `opponent country`
-  from final
+    f.id,
+    f.isvictory,
+    f.event,
+    f.`type`,
+    f.surface,
+    f.isoutdoor,
+    f.rank,
+    f.`date`,
+    f.locationid,
+    p1.name      as `player name`,
+    p1.country   as `player country`,
+    p2.name      as `opponent name`,
+    p2.country   as `opponent country`
+  from final f
   inner join player p1
-    on final.playerid = p1.id
+    on f.playerid = p1.id
   inner join player p2
-    on final.opponentid = p2.id
+    on f.opponentid = p2.id
+);
+
+
+
+drop view if exists by_eventtype;
+create view by_eventtype as (
+  select 
+    `player name`, 
+    `type` as `event type`,
+    sum(isvictory) as wins, 
+    count(*) - sum(isvictory) as losses,
+    count(*) as total
+  from joined_player 
+  group by `player name`, `type`
 );
 
 
@@ -34,11 +48,13 @@ create view joined_player as (
 drop view if exists by_opponent;
 create view by_opponent as (
   select
-    name, 
-    `opponent name`, sum(isvictory) as wins, 
-    count(*) - sum(isvictory) as losses, count(*) as total
+    `player name`, 
+    `opponent name`,
+    sum(isvictory) as wins, 
+    count(*) - sum(isvictory) as losses,
+    count(*) as total
   from joined_player 
-  group by name, `opponent name`
+  group by `player name`, `opponent name`
 );
 
 
@@ -46,153 +62,147 @@ create view by_opponent as (
 drop view if exists by_opponentcountry;
 create view by_opponentcountry as (
   select
-    name,
+    `player name`,
     `opponent country`,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
   from joined_player
-  group by name, `opponent country`
+  group by `player name`, `opponent country`
 );
 
 
 drop view if exists by_surface;
 create view by_surface as (
   select 
-    playerid,
-    surface, sum(isvictory) as wins,
-    count(*) - sum(isvictory) as losses, count(*) as total
-  from final
-  group by playerid, surface
+    `player name`,
+    surface,
+    sum(isvictory) as wins,
+    count(*) - sum(isvictory) as losses,
+    count(*) as total
+  from joined_player
+  group by `player name`, surface
 );
 
 
 drop view if exists by_inoutdoor;
 create view by_inoutdoor as (
   select
-    playerid,
+    `player name`,
     case when isoutdoor then "outdoor" else "indoor" end as "indoor/outdoor",
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
-  from final
-  group by playerid, isoutdoor
+  from joined_player
+  group by `player name`, isoutdoor
 );
 
 
 drop view if exists by_year;
 create view by_year as (
   select
-    playerid,
+    `player name`,
     year(date) as year,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
-  from final
-  group by playerid, year(date)
+  from joined_player
+  group by `player name`, year(date)
 );
 
 
 drop view if exists by_event;
 create view by_event as (
   select
-    playerid,
+    `player name`,
     event,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
-  from final
-  group by playerid, event
+  from joined_player
+  group by `player name`, event
 );
+
 
 
 drop view if exists joined_location;
 create view joined_location as (
   select 
-    f.*,
+    p.*,
     l.city,
     l.country,
     l.continent
-  from final f
+  from joined_player p
   inner join location l
-    on f.locationid = l.id
+    on p.locationid = l.id
 );
 
 
 drop view if exists by_city;
 create view by_city as (
   select
-    playerid, 
+    `player name`, 
     city,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
   from joined_location
-  group by playerid, city
+  group by `player name`, city
 );
 
 
 drop view if exists by_country;
 create view by_country as (
   select
-    playerid,
+    `player name`,
     country,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
   from joined_location
-  group by playerid, country
+  group by `player name`, country
 );
 
 
 drop view if exists by_continent;
 create view by_continent as (
   select
-    playerid,
+    `player name`,
     continent,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
   from joined_location
-  group by playerid, continent
+  group by `player name`, continent
 );
 
 
 drop view if exists by_rank;
 create view by_rank as (
   select
-    playerid, 
+    `player name`, 
     rank,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
-  from final
-  group by playerid, rank
+  from joined_location
+  group by `player name`, rank
 );
 
-
-
-drop view if exists joined_player_location;
-create view joined_player_location as (
-  select
-    l.name as `opponent name`,
-    l.country as `opponent country`,
-    r.*
-  from player l
-  inner join joined_location r
-    on l.id = r.opponentid
-);
 
 
 drop view if exists by_opponentsamecountry;
 create view by_opponentsamecountry as (
   select
-    case when `opponent country` = country then "same country" else "different country" end as `is same country?`,
+    case
+      when `opponent country` = `player country` 
+      then "same country" 
+      else "different country" end as `is same country?`,
     `opponent country`,
     sum(isvictory) as wins,
     count(*) - sum(isvictory) as losses,
     count(*) as total
-  from joined_player_location
+  from joined_location
   group by `is same country?`, `opponent country`
 );
 
